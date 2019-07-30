050dd1452eb611099958a1016891eda8aa43aee2
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-30 23:59:46 +0100

    feature: implement endpoints for income, create, read, delete for users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import userRoutes from './auth-routes';
+import userRoutes from './authRoutes';
+import incomeRoutes from './incomeRoutes'
 
 const route = express.Router();
 
 route.use('/auth', userRoutes);
 route.use('/users', userRoutes);
+route.use('/incomes', incomeRoutes);
 
 export default route;
